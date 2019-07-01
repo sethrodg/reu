393f9b71ed38cae08773c6d90e0a62911a1a2ecb
Seth Rodgers <sethallenrodgers@gmail.com>
2019-06-30 22:23:36 -0500

    nagivation to permissions updated

--- settingsCheck.py
+++ settingsCheck.py
@@ -2,6 +2,8 @@ import os
 import unittest
 from appium import webdriver
 from time import sleep
+from appium.webdriver.common.touch_action import TouchAction
+
 
 class settingsCheck(unittest.TestCase):
 
@@ -24,13 +26,30 @@ class settingsCheck(unittest.TestCase):
         #click 'apps and notifications'
         element = self.driver.find_elements_by_class_name("android.widget.LinearLayout")
         element[11].click()
+        sleep(2)
+
+        #scroll down
+        actions = TouchAction(self.driver)
+        actions.press(x=20, y=972).move_to(x=10, y=476).release().perform()
+        sleep(1)
+
+        #click 'advanced' and scroll down
+        element = self.driver.find_elements_by_class_name("android.widget.LinearLayout")
+        element[31].click()
+        sleep(1)
+        actions.press(x=20, y=972).move_to(x=10, y=476).release().perform()
+        sleep(2)
+
+        #click special app access
+        element = self.driver.find_elements_by_class_name("android.widget.RelativeLayout")
+        element[5].click()
+        sleep(2)
+
+        element = self.driver.find_elements_by_class_name("android.widget.RelativeLayout")
+        element[1].click()
         sleep(5)
 
-        #click 'advanced' - scroll first?
-        element = self.driver.find_elements_by_class_name("android.widget.RelativeLayout")
-        print(element)
-        element[2].click()
-        sleep(10)
+
 
 
 if __name__ == '__main__':
